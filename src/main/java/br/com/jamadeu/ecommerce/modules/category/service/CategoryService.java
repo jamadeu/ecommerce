a8fb047ab5b24ad2189d62dc2eb2c0c9441fea3c
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-22 08:44:50 -0300

    feat: CategoryService findByIdOrThrowBadRequestException

--- src/main/java/br/com/jamadeu/ecommerce/modules/category/service/CategoryService.java
+++ src/main/java/br/com/jamadeu/ecommerce/modules/category/service/CategoryService.java
@@ -2,6 +2,7 @@ package br.com.jamadeu.ecommerce.modules.category.service;
 
 import br.com.jamadeu.ecommerce.modules.category.domain.Category;
 import br.com.jamadeu.ecommerce.modules.category.repository.CategoryRepository;
+import br.com.jamadeu.ecommerce.shared.exception.BadRequestException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,5 +15,10 @@ public class CategoryService {
 
     public Page<Category> listAll(Pageable pageable) {
         return categoryRepository.findAll(pageable);
+    }
+
+    public Category findByIdOrThrowBadRequestException(long id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new BadRequestException("Category not found"));
     }
 }
