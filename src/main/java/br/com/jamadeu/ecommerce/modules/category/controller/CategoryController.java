e99272fb886c055001b4f3590a4ef1fcaa341f6d
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-22 08:43:44 -0300

    feat: CategoryController findById

--- src/main/java/br/com/jamadeu/ecommerce/modules/category/controller/CategoryController.java
+++ src/main/java/br/com/jamadeu/ecommerce/modules/category/controller/CategoryController.java
@@ -3,6 +3,8 @@ package br.com.jamadeu.ecommerce.modules.category.controller;
 import br.com.jamadeu.ecommerce.modules.category.domain.Category;
 import br.com.jamadeu.ecommerce.modules.category.service.CategoryService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
@@ -10,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +29,17 @@ public class CategoryController {
     )
     public ResponseEntity<Page<Category>> listAll(@ParameterObject Pageable pageable) {
         return new ResponseEntity<>(categoryService.listAll(pageable), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{id}")
+    @Operation(summary = "Find category by id",
+            tags = {"categories"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "When category does not found")
+    })
+    public ResponseEntity<Category> findById(@PathVariable long id) {
+        return new ResponseEntity<>(categoryService.findByIdOrThrowBadRequestException(id), HttpStatus.OK);
     }
 }
