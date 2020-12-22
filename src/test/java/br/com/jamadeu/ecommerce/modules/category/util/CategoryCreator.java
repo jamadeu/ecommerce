c90b2e0975feedc989a112836fb19df748344d43
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-22 09:30:54 -0300

    refact: remove EasyRandom

--- src/test/java/br/com/jamadeu/ecommerce/modules/category/util/CategoryCreator.java
+++ src/test/java/br/com/jamadeu/ecommerce/modules/category/util/CategoryCreator.java
@@ -1,16 +1,18 @@
 package br.com.jamadeu.ecommerce.modules.category.util;
 
 import br.com.jamadeu.ecommerce.modules.category.domain.Category;
-import org.jeasy.random.EasyRandom;
 
 public class CategoryCreator {
     public static Category createCategoryToBeSaved() {
-        Category category = new EasyRandom().nextObject(Category.class);
-        category.setId(null);
-        return category;
+        return Category.builder()
+                .category("category")
+                .build();
     }
 
     public static Category createValidCategory() {
-        return new EasyRandom().nextObject(Category.class);
+        return Category.builder()
+                .id(1L)
+                .category("category")
+                .build();
     }
 }
