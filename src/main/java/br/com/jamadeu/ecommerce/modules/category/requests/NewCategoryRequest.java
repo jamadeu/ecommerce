932a13699049cbbb08ce2971df0df60d1ac22c90
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-22 09:08:48 -0300

    feat: create NewCategoryRequest

---
+++ src/main/java/br/com/jamadeu/ecommerce/modules/category/requests/NewCategoryRequest.java
@@ -0,0 +1,20 @@
+package br.com.jamadeu.ecommerce.modules.category.requests;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewCategoryRequest {
+
+    @NotEmpty(message = "The user name can not be empty")
+    @Schema(description = "This is the category's name")
+    private String category;
+}
