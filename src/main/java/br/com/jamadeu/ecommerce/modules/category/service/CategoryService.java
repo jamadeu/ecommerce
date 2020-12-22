389ded1b514fb959e02a3c786c6ecd4e84458f0f
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-22 08:11:06 -0300

    feat: create CategoryService

---
+++ src/main/java/br/com/jamadeu/ecommerce/modules/category/service/CategoryService.java
@@ -0,0 +1,18 @@
+package br.com.jamadeu.ecommerce.modules.category.service;
+
+import br.com.jamadeu.ecommerce.modules.category.domain.Category;
+import br.com.jamadeu.ecommerce.modules.category.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public Page<Category> listAll(Pageable pageable) {
+        return categoryRepository.findAll(pageable);
+    }
+}
