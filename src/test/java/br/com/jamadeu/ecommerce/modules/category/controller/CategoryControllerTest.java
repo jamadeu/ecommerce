289fd215dafff15e1599f79a6c819ab68e0198e3
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-22 08:41:54 -0300

    test: CategoryController listAll test

---
+++ src/test/java/br/com/jamadeu/ecommerce/modules/category/controller/CategoryControllerTest.java
@@ -0,0 +1,52 @@
+package br.com.jamadeu.ecommerce.modules.category.controller;
+
+import br.com.jamadeu.ecommerce.modules.category.domain.Category;
+import br.com.jamadeu.ecommerce.modules.category.service.CategoryService;
+import br.com.jamadeu.ecommerce.modules.category.util.CategoryCreator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+class CategoryControllerTest {
+    @InjectMocks
+    private CategoryController categoryController;
+
+    @Mock
+    private CategoryService categoryServiceMock;
+
+    private Category category;
+
+    @BeforeEach
+    void setup() {
+        category = CategoryCreator.createValidCategory();
+        PageImpl<Category> categoryPage = new PageImpl<>(List.of(category));
+        BDDMockito.when(categoryServiceMock.listAll(ArgumentMatchers.any(PageRequest.class)))
+                .thenReturn(categoryPage);
+    }
+
+    @Test
+    @DisplayName("listAll returns list of categories inside page object when successful")
+    void listAll_ReturnsListOfCategoriesInsidePageObject_WhenSuccessful() {
+        String expectedCategory = category.getCategory();
+        Page<Category> categoryPage = categoryController.listAll(PageRequest.of(1, 1)).getBody();
+
+        Assertions.assertThat(categoryPage).isNotNull();
+        Assertions.assertThat(categoryPage.toList())
+                .isNotEmpty()
+                .hasSize(1);
+        Assertions.assertThat(categoryPage.toList().get(0).getCategory()).isEqualTo(expectedCategory);
+    }
+}
