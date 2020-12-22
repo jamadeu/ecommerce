7081a137ec55138bc9e0d28f6e80a5d8a57e4d76
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-22 07:26:23 -0300

    feat: create CategoryRepository

---
+++ src/main/java/br/com/jamadeu/ecommerce/modules/category/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package br.com.jamadeu.ecommerce.modules.category.repository;
+
+import br.com.jamadeu.ecommerce.modules.category.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByCategory(String category);
+}
