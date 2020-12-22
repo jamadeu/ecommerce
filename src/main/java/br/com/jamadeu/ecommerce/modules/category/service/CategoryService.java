5d52722579a9e2b78efa9cc58ce800bfe49f4c6c
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-22 09:09:04 -0300

    feat: CategoryService save

--- src/main/java/br/com/jamadeu/ecommerce/modules/category/service/CategoryService.java
+++ src/main/java/br/com/jamadeu/ecommerce/modules/category/service/CategoryService.java
@@ -1,12 +1,16 @@
 package br.com.jamadeu.ecommerce.modules.category.service;
 
 import br.com.jamadeu.ecommerce.modules.category.domain.Category;
+import br.com.jamadeu.ecommerce.modules.category.mapper.CategoryMapper;
 import br.com.jamadeu.ecommerce.modules.category.repository.CategoryRepository;
+import br.com.jamadeu.ecommerce.modules.category.requests.NewCategoryRequest;
 import br.com.jamadeu.ecommerce.shared.exception.BadRequestException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +24,18 @@ public class CategoryService {
     public Category findByIdOrThrowBadRequestException(long id) {
         return categoryRepository.findById(id)
                 .orElseThrow(() -> new BadRequestException("Category not found"));
+    }
+
+    @Transactional
+    public Category save(NewCategoryRequest newCategoryRequest) {
+        Category categoryToBeSaved = CategoryMapper.INSTANCE.toCategory(newCategoryRequest);
+        checkIfCategoryExists(categoryToBeSaved.getCategory());
+        return categoryRepository.save(categoryToBeSaved);
+    }
+
+    private void checkIfCategoryExists(String category) {
+        if (categoryRepository.findByCategory(category).isPresent()) {
+            throw new BadRequestException("Category already exists");
+        }
     }
 }
