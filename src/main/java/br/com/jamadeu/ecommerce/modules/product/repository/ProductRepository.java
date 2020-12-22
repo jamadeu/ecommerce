2003f2e68f0432c00f2b5ee412bf3bc92fbafe31
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-22 14:47:40 -0300

    chore: ProductRepository

--- src/main/java/br/com/jamadeu/ecommerce/modules/product/repository/ProductRepository.java
+++ src/main/java/br/com/jamadeu/ecommerce/modules/product/repository/ProductRepository.java
@@ -5,12 +5,11 @@ import br.com.jamadeu.ecommerce.modules.product.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findByName(String name);
 
-    List<Product> findByCategory(Category category);
+    Optional<Product> findByCategory(Category category);
 }
