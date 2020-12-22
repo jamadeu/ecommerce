a79726f1b325fcc7a9ec2aa180573968952cef03
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-22 08:48:24 -0300

    test: CateforyService findByIdOrThrowBadRequestException

--- src/test/java/br/com/jamadeu/ecommerce/modules/category/service/CategoryServiceTest.java
+++ src/test/java/br/com/jamadeu/ecommerce/modules/category/service/CategoryServiceTest.java
@@ -3,6 +3,7 @@ package br.com.jamadeu.ecommerce.modules.category.service;
 import br.com.jamadeu.ecommerce.modules.category.domain.Category;
 import br.com.jamadeu.ecommerce.modules.category.repository.CategoryRepository;
 import br.com.jamadeu.ecommerce.modules.category.util.CategoryCreator;
+import br.com.jamadeu.ecommerce.shared.exception.BadRequestException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -18,6 +19,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 class CategoryServiceTest {
@@ -35,6 +37,8 @@ class CategoryServiceTest {
         PageImpl<Category> categoryPage = new PageImpl<>(List.of(category));
         BDDMockito.when(categoryRepositoryMock.findAll(ArgumentMatchers.any(PageRequest.class)))
                 .thenReturn(categoryPage);
+        BDDMockito.when(categoryRepositoryMock.findById(ArgumentMatchers.anyLong()))
+                .thenReturn(Optional.of(category));
     }
 
     @Test
@@ -48,6 +52,27 @@ class CategoryServiceTest {
                 .isNotEmpty()
                 .hasSize(1);
         Assertions.assertThat(categoryPage.toList().get(0).getCategory()).isEqualTo(expectedCategory);
+    }
+
+    @Test
+    @DisplayName("findByIdOrThrowBadRequestException returns category when successful")
+    void findByIdOrThrowBadRequestException_ReturnsCategory_WhenSuccessful() {
+        Long expectedId = category.getId();
+        Category foundedCategory = categoryService.findByIdOrThrowBadRequestException(1);
+
+        Assertions.assertThat(foundedCategory).isNotNull();
+        Assertions.assertThat(foundedCategory.getId())
+                .isNotNull()
+                .isEqualTo(expectedId);
+    }
+
+    @Test
+    @DisplayName("findByIdOrThrowBadRequestException throws BadRequestException when category is not found")
+    void findByIdOrThrowBadRequestException_ThrowsBadRequestException_WhenCategoryIsNotFound() {
+        BDDMockito.when(categoryRepositoryMock.findById(ArgumentMatchers.anyLong()))
+                .thenReturn(Optional.empty());
+        Assertions.assertThatExceptionOfType(BadRequestException.class)
+                .isThrownBy(() -> categoryService.findByIdOrThrowBadRequestException(1));
     }
 
 }
