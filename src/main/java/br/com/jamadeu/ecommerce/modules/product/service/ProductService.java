535820fbc2d40cb815aac08be09ff9653541ef46
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-24 09:46:40 -0300

    feat: ProductService create

--- src/main/java/br/com/jamadeu/ecommerce/modules/product/service/ProductService.java
+++ src/main/java/br/com/jamadeu/ecommerce/modules/product/service/ProductService.java
@@ -3,7 +3,9 @@ package br.com.jamadeu.ecommerce.modules.product.service;
 import br.com.jamadeu.ecommerce.modules.category.domain.Category;
 import br.com.jamadeu.ecommerce.modules.category.repository.CategoryRepository;
 import br.com.jamadeu.ecommerce.modules.product.domain.Product;
+import br.com.jamadeu.ecommerce.modules.product.mapper.ProductMapper;
 import br.com.jamadeu.ecommerce.modules.product.repository.ProductRepository;
+import br.com.jamadeu.ecommerce.modules.product.requests.NewProductRequest;
 import br.com.jamadeu.ecommerce.shared.exception.BadRequestException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -11,6 +13,8 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -33,5 +37,27 @@ public class ProductService {
     public Product findByIdOrThrowBadRequestException(Long id) {
         return productRepository.findById(id)
                 .orElseThrow(() -> new BadRequestException("Product not found"));
+    }
+
+    @Transactional
+    public Product create(NewProductRequest newProductRequest) {
+        Product productToCreate = ProductMapper.INSTANCE.toProduct(newProductRequest);
+        checkIfCategoryExists(productToCreate.getCategory().getCategory());
+        checkIfProductExists(productToCreate.getName());
+        productToCreate.setValue(BigDecimal.ZERO);
+        return productRepository.save(productToCreate);
+
+    }
+
+    private void checkIfCategoryExists(String category) {
+        if (categoryRepository.findByCategory(category).isPresent()) {
+            throw new BadRequestException("Category already exists");
+        }
+    }
+
+    private void checkIfProductExists(String product) {
+        if (productRepository.findByName(product).isPresent()) {
+            throw new BadRequestException("Product already exists");
+        }
     }
 }
