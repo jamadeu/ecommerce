35e926ca14c2ab91e9aaaabadcc1c9cd382e1150
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-24 09:06:40 -0300

    chore: ProductController listAllByCategory check if category is not null

--- src/main/java/br/com/jamadeu/ecommerce/modules/product/controller/ProductController.java
+++ src/main/java/br/com/jamadeu/ecommerce/modules/product/controller/ProductController.java
@@ -3,6 +3,7 @@ package br.com.jamadeu.ecommerce.modules.product.controller;
 import br.com.jamadeu.ecommerce.modules.category.service.CategoryService;
 import br.com.jamadeu.ecommerce.modules.product.domain.Product;
 import br.com.jamadeu.ecommerce.modules.product.service.ProductService;
+import br.com.jamadeu.ecommerce.shared.exception.BadRequestException;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.api.annotations.ParameterObject;
@@ -14,8 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("products")
@@ -33,12 +32,13 @@ public class ProductController {
         return new ResponseEntity<>(productService.listAll(pageable), HttpStatus.OK);
     }
 
-    @GetMapping(path = "/{category}")
+    @GetMapping(path = "/list-by-category/{category}")
     @Operation(summary = "List all products by category paginated",
             description = "The default size is 5, use the parameter to change the default value",
             tags = {"products"}
     )
-    public ResponseEntity<Page<Product>> listAllByCategory(@ParameterObject Pageable pageable, @PathVariable @NotNull String category) {
+    public ResponseEntity<Page<Product>> listAllByCategory(@ParameterObject Pageable pageable, @PathVariable String category) {
+        if (category == null) throw new BadRequestException("Category can not be null");
         return new ResponseEntity<>(productService.listAllByCategory(pageable, category), HttpStatus.OK);
     }
 }
