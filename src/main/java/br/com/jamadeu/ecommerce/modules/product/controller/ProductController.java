fc5baf693a9e6871cb5b11767f785546bc78f370
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-24 09:46:30 -0300

    feat: ProductController create

--- src/main/java/br/com/jamadeu/ecommerce/modules/product/controller/ProductController.java
+++ src/main/java/br/com/jamadeu/ecommerce/modules/product/controller/ProductController.java
@@ -1,6 +1,7 @@
 package br.com.jamadeu.ecommerce.modules.product.controller;
 
 import br.com.jamadeu.ecommerce.modules.product.domain.Product;
+import br.com.jamadeu.ecommerce.modules.product.requests.NewProductRequest;
 import br.com.jamadeu.ecommerce.modules.product.service.ProductService;
 import br.com.jamadeu.ecommerce.shared.exception.BadRequestException;
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,10 +13,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("products")
@@ -52,10 +52,24 @@ public class ProductController {
     )
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "Successful operation"),
-            @ApiResponse(responseCode = "400", description = "When product is not foundd")
+            @ApiResponse(responseCode = "400", description = "When product is not found")
     })
     public ResponseEntity<Product> findById(@PathVariable Long id) {
         return new ResponseEntity<>(productService.findByIdOrThrowBadRequestException(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    @Operation(summary = "Create a new product",
+            description = "Name, description and category fields are mandatory, " +
+                    "name must be unique",
+            tags = {"products"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "When there is an error with some mandatory field")
+    })
+    public ResponseEntity<Product> create(@RequestBody @Valid NewProductRequest newProductRequest) {
+        return new ResponseEntity<>(productService.create(newProductRequest), HttpStatus.CREATED);
     }
 
 
